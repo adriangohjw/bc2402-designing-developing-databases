cb25be25926b66d004ad09d43f3130f116f071a0
deionna <64540745+deionna@users.noreply.github.com>
2021-11-05 23:30:13 +0800

    normalisation script

---
+++ normalisation of relational database.sql
@@ -0,0 +1,55 @@
+# CREATE location TABLE
+CREATE TABLE location
+AS (SELECT location, iso_code, continent, population_density, median_age, aged_65_older, 
+	aged_70_older, gdp_per_capita, extreme_poverty, cardiovasc_death_rate, diabetes_prevalence, 
+	female_smokers, male_smokers, handwashing_facilities, hospital_beds_per_thousand, life_expectancy, 
+    human_development_index
+    FROM covid19data
+    GROUP BY location);
+
+# CREATE covid_cases TABLE
+CREATE TABLE covid_cases
+AS (SELECT location, date, total_cases, new_cases, new_cases_smoothed, reproduction_rate
+	FROM covid19data
+    WHERE new_cases <> '' AND total_cases <> '' OR new_cases_smoothed <> ''
+    GROUP BY location, date);
+
+#CREATE covid_deaths TABLE
+CREATE TABLE covid_deaths
+AS (SELECT location, date, total_deaths_per_million, new_deaths_per_million, excess_mortality
+	FROM covid19data
+    WHERE total_deaths_per_million <> '' OR new_deaths_per_million <> '' OR excess_mortality <> ''
+    GROUP BY location, date);
+
+# CREATE covid_hospitalisation TABLE
+CREATE TABLE covid_hospitalisation
+AS (SELECT location, date, icu_patients, icu_patients_per_million, hosp_patients, 
+	hosp_patients_per_million, weekly_icu_admissions, weekly_icu_admissions_per_million, weekly_hosp_admissions, 
+	weekly_hosp_admissions_per_million
+	FROM covid19data
+	WHERE hosp_patients <> '' OR icu_patients <> '' OR weekly_hosp_admissions <> '' OR weekly_icu_admissions <> ''
+	GROUP BY location, date);
+
+# CREATE covid_tests TABLE
+CREATE TABLE covid_tests
+AS (SELECT location, date, new_tests, total_tests, total_tests_per_thousand, new_tests_per_thousand, new_tests_smoothed, 
+	new_tests_smoothed_per_thousand, positive_rate, tests_per_case, tests_units
+    FROM covid19data
+    WHERE new_tests <> '' OR new_tests_smoothed <> '' OR total_tests <> ''
+    GROUP BY location, date);
+
+# CREATE covid_vaccinations TABLE
+CREATE TABLE covid_vaccinations
+AS (SELECT location, date, total_vaccinations, people_vaccinated, people_fully_vaccinated, new_vaccinations, 
+	new_vaccinations_smoothed, total_vaccinations_per_hundred, people_vaccinated_per_hundred, people_fully_vaccinated_per_hundred, 
+	new_vaccinations_smoothed_per_million
+	FROM covid19data
+	WHERE new_vaccinations <> '' AND total_vaccinations <> '' AND people_vaccinated <> '' AND new_vaccinations_smoothed <> ''
+	GROUP BY location, date);
+    
+# CREATE covid_measures TABLE
+CREATE TABLE covid_measures
+AS (SELECT location, date, stringency_index
+	FROM covid19data
+    WHERE stringency_index <> ''
+    GROUP BY location, date);
